8bfe785ff5756122eb7671ad0c3c7029f2e6714b
박소연 <soya@bagsoyeon-ui-MacBookPro.local>
2020-06-27 17:02:10 +0900

    update for delete file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-        <h3>Hello </h3>
-    </div>
-  );
+  return <div>HELLO</div>
 }
 
 export default App;
